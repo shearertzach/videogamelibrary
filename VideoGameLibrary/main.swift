4551f32cdfcb4ec63f0e2c53034abe8b153605ea
Zach Shearer <zachshearer@interapts-macbook-pro-7.local>
2018-08-30 10:48:21 -0500

    Initial Commit

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Zach Shearer on 8/30/18.
+//  Copyright © 2018 Zach Shearer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
